0e4434db7b3e45a10acd0c6fc4272abd726bce2a
Connor Fitzpatrick <fitzco01@connors-macbook-air.local>
2016-04-04 18:56:45 -0500

    Initial Commit

---
+++ Cassini/DemoURL.swift
@@ -0,0 +1,18 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Connor Fitzpatrick on 4/4/16.
+//  Copyright © 2016 Connor Fitzpatrick. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let Luther = NSURL(string: "http://www.luther.edu/reason/sized_images/551588/4811a85d604b4d4dac88311903f70864.jpg")
+    struct NASA {
+        static let Cassini = NSURL(string: "http://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")
+        static let Earth = NSURL(string: "http://www.nasa.gov/sites/default/files/wave_earth_mosaic_3.jpg")
+        static let Saturn = NSURL(string: "http://www.nasa.gov/sites/default/files/saturn_collage.jpg")
+    }
+}
